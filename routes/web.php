926932beecc665ddf792795cf324c3dc547ece3f
Deva <theprojessa@gmail.com>
2022-11-01 22:29:34 +0700

    install template admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/login', function () {
+        return view('admin.login');
+    });
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
