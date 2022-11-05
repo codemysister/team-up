be906e4af9e44f5b1808d996aa7307e927c031d0
apasii <20102172@ittelkom-pwt.ac.id>
2022-11-05 10:13:19 +0100

    remove dark mode

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -30,11 +30,11 @@
                 <div class="flex items-start mt-2">
                   <div class="flex items-start">
                       <div class="flex items-center h-5">
-                          <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                          <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300    " required>
                       </div>
-                      <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                      <label for="remember" class="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
                   </div>
-                  <a href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                  <a href="#" class="ml-auto text-sm text-blue-700 hover:underline ">Lost Password?</a>
               </div>
 
                 <button type="submit" class="w-full block bg-purple-500 hover:bg-purple-400 focus:bg-purple-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">
