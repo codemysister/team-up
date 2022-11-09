10d0b445ea3cab6ac47080a301382ea726e5f975
apasii <20102172@ittelkom-pwt.ac.id>
2022-11-09 15:09:45 +0100

    edit page verifikasi

--- resources/views/user/auth/verifikasi.blade.php
+++ resources/views/user/auth/verifikasi.blade.php
@@ -6,26 +6,23 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   @vite('resources/css/app.css')
 </head>
-<body class="bg-[#FFF5EC]">
+<body class="bg-[#FFF5EC] ">
 
     
-    <div class="container mx-auto -mt-16 sm:-mt-28   md:text-2xl  lg:text-2xl">
+    <div class="container  mx-auto -mt-16 sm:-mt-28   md:text-2xl  lg:text-2xl">
         
-        <img class="w-full h-full mt-[20%] sm:mt-0 md:w-[600px] md:mx-auto lg:w-[600px] lg:-mt-20 lg:mx-auto" src="{{asset('img/email-verification.png')}}">
-        <h1 class="font-poppins text-3xl px-5 text-center -mt-16 mb-2 font-bold md:text-5xl">Confirm your email </h1>
-        <h1 class="font-base font-poppins  text-center px-6 mx-auto -mt-13 text-xl md:text-2xl">We have sent a verification email to <span class="text-gray-600 font-poppins font-bold text">deva@gmail.com</span>, please check your email and complete the verification process to proceed to the next step
-        </h1>
-{{-- 
-        <div class="flex mt-20 w-full justify-center">
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-[#FFF5EC] dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Resend Email
-            </span>
-          </button>
+        <img class="w-full h-full mt-[10%] sm:mt-0 md:w-[600px] md:mx-auto lg:w-[600px] lg:-mt-20 lg:mx-auto" src="{{asset('img/email-verification.png')}}">
+        <h1 class="font-poppins text-3xl px-5 text-center -mt-16 sm:-mt-28 mb-2 font-bold md:text-4xl">Confirm your email </h1>
+        <p class="font-base font-poppins  text-center px-1  mx-auto -mt-13 text-xl md:text-2xl  md:text-center ">We have sent verification email to</p>
+        <p class=" text-gray-800 font-poppins font-bold text-center text-xl my-1">deva@gmail.com</p>
+        <p class="font-base font-poppins  text-justify px-4 mx-auto -mt-13 text-xl md:text-2xl sm:text-center lg:w-[600px] ">please check your email and complete the verification process to proceed to the next step</p>
+
+       
+        <div class="ml-[30%] sm:ml-[40%] lg:ml-[45%]  mt-5">
+          <button type="button" class="text-white   focus:ring-4 focus:outline-none focus:ring-pink-200 bg-orange-500 hover:bg-orange-600 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2">Resend Email</button>
 
         </div>
-         --}}
-
+       
         
 
     </div>
