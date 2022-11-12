a6579553feb85f4d242817aeebee2760790597f8
apasii <20102172@ittelkom-pwt.ac.id>
2022-11-12 12:34:53 +0100

    page profile

--- resources/views/user/team-list.blade.php
+++ resources/views/user/team-list.blade.php
@@ -97,6 +97,7 @@
               <div>
               </div>
               <div class="">
+                <a href=""></a>
                 <button type="button" class="text-white  bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="{{url('/team-detail')}}">Detail</a></button>
               </div>
             </div>
